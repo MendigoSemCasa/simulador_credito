c293e913ac6847fcdb4597cadb675b0ca8d8e331
Giordano Bruno <giordanobgv@gmail.com>
2023-06-04 08:51:49 -0300

    Initial Commit

---
+++ simulador_credito/simulador_creditoApp.swift
@@ -0,0 +1,17 @@
+//
+//  simulador_creditoApp.swift
+//  simulador_credito
+//
+//  Created by Giordano Bruno on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct simulador_creditoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
